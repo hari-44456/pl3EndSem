37f7e4dba859d4354d6c9c4c74286b8a70eaabb5
hari-44456 <naraharipapshetwar@gmail.com>
2021-01-01 17:54:37 +0700

    added validation to html and css

---
+++ signup.js
@@ -0,0 +1,10 @@
+
+
+const validate = () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+        document.getElementById('showAlert').innerHTML=`<div class="alert alert-warning alert-dismissible fade show" role="alert">
+        Sign Up  Successful
+      </div>`
+    
+}
